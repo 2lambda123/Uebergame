322a94a66d0cabc999803c5e7d24b4641574d678
Duion <duionum@gmail.com>
2015-05-09 15:07:51 +0200

    Merge 0.02 with master

---
+++ art/shapes/walls/invisible/materials.cs
@@ -0,0 +1,10 @@
+
+singleton Material(invisible_cube_1x1_Material)
+{
+   mapTo = "Material";
+   diffuseColor[0] = "0.64 0.64 0.64 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "50";
+   doubleSided = "1";
+   translucentBlendOp = "None";
+};
