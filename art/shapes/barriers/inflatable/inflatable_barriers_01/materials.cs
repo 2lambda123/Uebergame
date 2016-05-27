94b369c8f51b347886dbd07781a6fed39ef11137
Duion <duionium@gmail.com>
2016-05-27 14:39:40 +0200

    inflatable_barriers_01 added for paintball

---
+++ art/shapes/barriers/inflatable/inflatable_barriers_01/materials.cs
@@ -0,0 +1,78 @@
+singleton Material(Mat_inflatable_barrier_01)
+{
+   mapTo = "base_inflatable_barrier_01";
+   diffuseColor[0] = "1 1 1 1";
+   specular[0] = "0.5 0.5 0.5 1";
+   specularPower[0] = "16";
+   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_base_D.dds";
+   specularStrength[0] = "0.7";
+   pixelSpecular[0] = "0";
+   showFootprints = "0";
+   normalMap[0] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_N.dds";
+   detailScale[0] = "1 1";
+   diffuseColor[1] = "1 1 1 0.8";
+   diffuseMap[1] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_base_D.dds";
+   specular[1] = "0.603922 0.603922 0.603922 1";
+   specularPower[1] = "16";
+   specularStrength[1] = "0.7";
+   pixelSpecular[1] = "1";
+   cubemap = "sky_day_01_Cubemap";
+};
+
+singleton Material(Mat_blueyellow_inflatable_barrier_01 : Mat_inflatable_barrier_01)
+{
+   mapTo = "blueyellow_inflatable_barrier_01";
+   diffuseMap[0] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_blueyellow_D.dds";
+   diffuseColor[1] = "1 1 1 0.8";
+   diffuseMap[1] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_blueyellow_D.dds";
+   specular[1] = "0.6 0.6 0.6 1";
+   specularPower[1] = "16";
+   specularStrength[1] = "0.7";
+   pixelSpecular[1] = "1";
+   cubemap = "sky_day_01_Cubemap";
+   pixelSpecular[0] = "0";
+};
+
+singleton Material(Mat_redblue_inflatable_barrier_01 : Mat_inflatable_barrier_01)
+{
+   mapTo = "redblue_inflatable_barrier_01";
+   diffuseMap[0] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_redblue_D.dds";
+   cubemap = "sky_day_01_Cubemap";
+   pixelSpecular[0] = "0";
+   alphaRef = "0";
+   diffuseColor[1] = "0.803922 0.803922 0.803922 0.809";
+   diffuseMap[1] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_redblue_D.dds";
+   specular[1] = "0.611765 0.607843 0.607843 1";
+   specularPower[1] = "16";
+   specularStrength[1] = "0.7";
+   pixelSpecular[1] = "1";
+};
+
+singleton Material(Mat_blackyellow_inflatable_barrier_01 : Mat_inflatable_barrier_01)
+{
+   mapTo = "blackyellow_inflatable_barrier_01";
+   diffuseMap[0] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_blackyellow_D.dds";
+   diffuseColor[1] = "1 1 1 0.8";
+   diffuseMap[1] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_blackyellow_D.dds";
+   specular[1] = "0.603922 0.603922 0.603922 1";
+   specularPower[1] = "16";
+   specularStrength[1] = "0.7";
+   pixelSpecular[1] = "1";
+   cubemap = "sky_day_01_Cubemap";
+   pixelSpecular[0] = "0";
+};
+
+singleton Material(Mat_greenblack_inflatable_barrier_01 : Mat_inflatable_barrier_01)
+{
+   mapTo = "greenblack_inflatable_barrier_01";
+   diffuseMap[0] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_greenblack_D.dds";
+   diffuseColor[1] = "1 1 1 0.8";
+   diffuseMap[1] = "art/shapes/barriers/inflatable/inflatable_barriers_01/inflatable_barrier_01_greenblack_D.dds";
+   specular[1] = "0.6 0.6 0.6 1";
+   specularPower[1] = "16";
+   specularStrength[1] = "0.7";
+   pixelSpecular[1] = "1";
+   cubemap = "sky_day_01_Cubemap";
+   pixelSpecular[0] = "0";
+};
