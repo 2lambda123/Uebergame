a23a43626e22733297fcbee0a95974eb5cc01ad7
Duion <duion@cumallover.me>
2017-12-17 04:46:12 +0100

    postFX: better postFX preset

--- scripts/client/postFx/default.postfxpreset.cs
+++ scripts/client/postFx/default.postfxpreset.cs
@@ -1,73 +1,56 @@
-//-----------------------------------------------------------------------------
-// Copyright (c) 2012 GarageGames, LLC
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
-//-----------------------------------------------------------------------------
-
+$PostFXManager::Settings::EnablePostFX = "1";
+$PostFXManager::Settings::EnableSSAO = "1";
+$PostFXManager::Settings::EnableHDR = "1";
+$PostFXManager::Settings::EnableLightRays = "1";
+$PostFXManager::Settings::EnableDOF = "1";
 $PostFXManager::Settings::EnableVignette = "1";
 $PostFXManager::Settings::EnableSunBokeh = "0";
-$PostFXManager::Settings::EnableDOF = "0";
-$PostFXManager::Settings::EnableSSAO = "0";
-$PostFXManager::Settings::EnableHDR = "1";
-$PostFXManager::Settings::EnableLightRays = "0";
-$PostFXManager::Settings::EnablePostFX = "1";
-$PostFXManager::Settings::Vignette::VMax = "2.0";
-$PostFXManager::Settings::DOF::BlurCurveFar = "";
-$PostFXManager::Settings::DOF::BlurCurveNear = "";
-$PostFXManager::Settings::DOF::BlurMax = "";
-$PostFXManager::Settings::DOF::BlurMin = "";
-$PostFXManager::Settings::DOF::EnableAutoFocus = "";
-$PostFXManager::Settings::DOF::FocusRangeMax = "";
-$PostFXManager::Settings::DOF::FocusRangeMin = "";
+$PostFXManager::Settings::ColorCorrectionRamp = "scripts/client/postFx/null_color_ramp.png";
+$PostFXManager::Settings::DOF::BlurCurveFar = "50";
+$PostFXManager::Settings::DOF::BlurCurveNear = "50";
+$PostFXManager::Settings::DOF::BlurMax = "0.15";
+$PostFXManager::Settings::DOF::BlurMin = "0.07";
+$PostFXManager::Settings::DOF::EnableAutoFocus = "1";
+$PostFXManager::Settings::DOF::FocusRangeMax = "500";
+$PostFXManager::Settings::DOF::FocusRangeMin = "100";
 $PostFXManager::Settings::HDR::adaptRate = "2";
-$PostFXManager::Settings::HDR::blueShiftColor = "1.05 0.97 1.27";
-$PostFXManager::Settings::HDR::brightPassThreshold = "1";
+$PostFXManager::Settings::HDR::blueShiftColor = "0.5 0.85 0.95 1";
+$PostFXManager::Settings::HDR::brightPassThreshold = "1.2";
 $PostFXManager::Settings::HDR::enableBloom = "1";
-$PostFXManager::Settings::HDR::enableBlueShift = "0";
-$PostFXManager::Settings::HDR::enableToneMapping = "0";
-$PostFXManager::Settings::HDR::gaussMean = "0";
-$PostFXManager::Settings::HDR::gaussMultiplier = "0.3";
-$PostFXManager::Settings::HDR::gaussStdDev = "0.8";
-$PostFXManager::Settings::HDR::keyValue = "0.18";
-$PostFXManager::Settings::HDR::minLuminace = "0.001";
-$PostFXManager::Settings::HDR::whiteCutoff = "1";
+$PostFXManager::Settings::HDR::enableBlueShift = "0.2";
+$PostFXManager::Settings::HDR::enableToneMapping = "0.3";
+$PostFXManager::Settings::HDR::gaussMean = "0.1";
+$PostFXManager::Settings::HDR::gaussMultiplier = "0.2";
+$PostFXManager::Settings::HDR::gaussStdDev = "3";
+$PostFXManager::Settings::HDR::keyValue = "0.4";
+$PostFXManager::Settings::HDR::minLuminace = "0";
+$PostFXManager::Settings::HDR::whiteCutoff = "0";
 $PostFXManager::Settings::LightRays::brightScalar = "0.75";
-$PostFXManager::Settings::LightRays::decay = "1.0";
-$PostFXManager::Settings::LightRays::density = "0.94";
-$PostFXManager::Settings::LightRays::numSamples = "40";
-$PostFXManager::Settings::LightRays::weight = "5.65";
-$PostFXManager::Settings::SSAO::blurDepthTol = "0.001";
+$PostFXManager::Settings::LightRays::decay = "1";
+$PostFXManager::Settings::LightRays::density = "0.80";
+$PostFXManager::Settings::LightRays::numSamples = "20";
+$PostFXManager::Settings::LightRays::weight = "8";
+$PostFXManager::Settings::SSAO::blurDepthTol = "0.3";
 $PostFXManager::Settings::SSAO::blurNormalTol = "0.95";
-$PostFXManager::Settings::SSAO::lDepthMax = "2";
-$PostFXManager::Settings::SSAO::lDepthMin = "0.2";
+$PostFXManager::Settings::SSAO::lDepthMax = "3";
+$PostFXManager::Settings::SSAO::lDepthMin = "0.3";
 $PostFXManager::Settings::SSAO::lDepthPow = "0.2";
 $PostFXManager::Settings::SSAO::lNormalPow = "2";
 $PostFXManager::Settings::SSAO::lNormalTol = "-0.5";
-$PostFXManager::Settings::SSAO::lRadius = "1";
-$PostFXManager::Settings::SSAO::lStrength = "10";
-$PostFXManager::Settings::SSAO::overallStrength = "2";
-$PostFXManager::Settings::SSAO::quality = "0";
-$PostFXManager::Settings::SSAO::sDepthMax = "1";
-$PostFXManager::Settings::SSAO::sDepthMin = "0.1";
+$PostFXManager::Settings::SSAO::lRadius = "0.4";
+$PostFXManager::Settings::SSAO::lStrength = "2";
+$PostFXManager::Settings::SSAO::overallStrength = "10";
+$PostFXManager::Settings::SSAO::quality = "2";
+$PostFXManager::Settings::SSAO::sDepthMax = "3.8";
+$PostFXManager::Settings::SSAO::sDepthMin = "0.5";
 $PostFXManager::Settings::SSAO::sDepthPow = "1";
 $PostFXManager::Settings::SSAO::sNormalPow = "1";
 $PostFXManager::Settings::SSAO::sNormalTol = "0";
-$PostFXManager::Settings::SSAO::sRadius = "0.1";
-$PostFXManager::Settings::SSAO::sStrength = "6";
-$PostFXManager::Settings::ColorCorrectionRamp = "scripts/client/postFx/null_color_ramp.png";
+$PostFXManager::Settings::SSAO::sRadius = "0.15";
+$PostFXManager::Settings::SSAO::sStrength = "2.7";
+$PostFXManager::Settings::SunBokeh::bokehSize = "550";
+$PostFXManager::Settings::SunBokeh::edgeAmount = "0.3";
+$PostFXManager::Settings::SunBokeh::haloAmount = "0.3";
+$PostFXManager::Settings::SunBokeh::sunAmount = "0.3";
+$PostFXManager::Settings::SunBokeh::sunSize = "0.125";
+$PostFXManager::Settings::Vignette::VMax = "2";
