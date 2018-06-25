b1772732609d3afae011fccb08914a16e4170742
Duion <duion@cumallover.me>
2018-06-26 00:09:46 +0200

    art/shapes: flag_stand_01 material adjustments

--- art/shapes/objectives/flag_stands/materials.cs
+++ art/shapes/objectives/flag_stands/materials.cs
@@ -2,12 +2,8 @@
 singleton Material(Mat_flag_stand_01)
 {
    mapTo = "base_flag_stand_01";
-   diffuseColor[0] = "0.666667 0.666667 0.666667 1";
-   specular[0] = "0.5 0.5 0.5 1";
-   specularPower[0] = "1";
-   translucentBlendOp = "None";
+   diffuseMap[0] = "art/shapes/objectives/flag_stands/flag_stand_01_D.dds";
    normalMap[0] = "art/shapes/objectives/flag_stands/flag_stand_01_N.dds";
-   specularStrength[0] = "1";
    specularMap[0] = "art/shapes/objectives/flag_stands/flag_stand_01_S.dds";
    showFootprints = "0";
 };
@@ -15,29 +11,29 @@ singleton Material(Mat_flag_stand_01)
 singleton Material(Mat_blue_flag_stand_01 : Mat_flag_stand_01)
 {
    mapTo = "blue_flag_stand_01";
-   diffuseMap[0] = "art/textures/colors/team_blue_01.dds";
+   diffuseColor[0] = "0.188235 0.439216 0.8 1";
 };
 
 singleton Material(Mat_red_flag_stand_01 : Mat_flag_stand_01)
 {
    mapTo = "red_flag_stand_01";
-   diffuseMap[0] = "art/textures/colors/team_red_01.dds";
+   diffuseColor[0] = "0.608 0.176 0.188 1";
 };
 
 singleton Material(Mat_green_flag_stand_01 : Mat_flag_stand_01)
 {
    mapTo = "green_flag_stand_01";
-   diffuseMap[0] = "art/textures/colors/team_green_01.dds";
+   diffuseColor[0] = "0.247 0.439 0.11 1";
 };
 
 singleton Material(Mat_yellow_flag_stand_01 : Mat_flag_stand_01)
 {
    mapTo = "yellow_flag_stand_01";
-   diffuseMap[0] = "art/textures/colors/team_yellow_01.dds";
+   diffuseColor[0] = "0.918 0.678 0.078 1";
 };
 
 singleton Material(Mat_black_flag_stand_01 : Mat_flag_stand_01)
 {
    mapTo = "black_flag_stand_01";
-   diffuseMap[0] = "art/textures/colors/team_black_01.dds";
+   diffuseColor[0] = "0.128 0.128 0.128 1";
 };
